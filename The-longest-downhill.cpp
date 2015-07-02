cfdcf23a807b451f34378876fbcacb1b867b7ed7
patrykh <holda.patryk@gmail.com>
2015-07-02 20:08:23 +0200

    dane

--- The-longest-downhill.cpp
+++ The-longest-downhill.cpp
@@ -1,12 +1,22 @@
 // The-longest-downhill.cpp : Defines the entry point for the console application.
-//
+// Wczytywanie danych
+// Iloœc wêz³ów <-- na tej podtawie pêtla for do wpisywania listy wez³ów
+// wêze³ startowy
+// lista wêz³ów
+// Graf skierowany, wa¿ony
+// Alg Dijkstry???
 
 #include "stdafx.h"
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
-	cout << "Hello wordl";
+	int ilosc_wierzcholkow = 0,
+		start = 0,
+		ilosc_sasiadow = 0,
+		sasiad = 0,
+		dlugosc_krawedzi = 0;
+
 	return 0;
 }
 
