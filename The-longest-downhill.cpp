59de37b32d5fb06d9e86c460874fe7fac66fad1c
patrykh <holda.patryk@gmail.com>
2015-07-03 18:25:46 +0200

    Edycja

--- The-longest-downhill.cpp
+++ The-longest-downhill.cpp
@@ -1,13 +1,15 @@
 // The-longest-downhill.cpp : Defines the entry point for the console application.
 // Wczytywanie danych
-// Iloœc wêz³ów <-- na tej podtawie pêtla for do wpisywania listy wez³ów
+// OK - Iloœc wêz³ów <-- na tej podtawie pêtla for do wpisywania listy wez³ów
 // wêze³ startowy
 // lista wêz³ów
 // Graf skierowany, wa¿ony
-// Alg Dijkstry???
+// Alg Dijkstry z wagami ujemnymi???
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
@@ -15,7 +17,56 @@ int main()
 		start = 0,
 		ilosc_sasiadow = 0,
 		sasiad = 0,
-		dlugosc_krawedzi = 0;
+		waga = 0;
+	
+	cin >> ilosc_wierzcholkow >> start;
+
+	//Tworzenie macierzy
+	char ** A = new char*[ilosc_wierzcholkow];
+	for (int i = 0; i < ilosc_wierzcholkow; i++){
+		A[i] = new char[ilosc_wierzcholkow];
+	}
+
+	//wypisywanie zerami
+	for (int i = 0; i < ilosc_wierzcholkow; i++){
+		for (int j = 0; j < ilosc_wierzcholkow; j++){
+			A[i][j] = 0;
+		}
+	}
+
+	//char ??
+	//for (int m = 0; m < ilosc_wierzcholkow; m++){
+	//	cin >> ilosc_sasiadow;
+	//	for (int n = 0; n < ilosc_sasiadow; n++){
+	//		cin >> sasiad >> waga;
+	//		a[m][sasiad] = '1';
+	//	}
+	//}
+
+	// INT ?? 
+	for (int m = 0; m < ilosc_wierzcholkow; m++){
+		cin >> ilosc_sasiadow;
+		for (int n = 0; n < ilosc_sasiadow; n++){
+			cin >> sasiad >> waga;
+			A[m][sasiad] = waga;
+		}
+	}
+
+	//wyœwietlanie macierzy
+	cout << "-   ";
+	for (int i = 0; i < ilosc_wierzcholkow; i++) cout << i << " ";
+	cout << endl << endl;
+	for (int i = 0; i < ilosc_wierzcholkow; i++){
+		cout << i << "   ";
+		for (int j = 0; j < ilosc_wierzcholkow; j++){
+			cout << A[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	//Czyszczenie pamiêci
+	for (int i = 0; i < ilosc_wierzcholkow; i++) delete[] A[i];
+	delete[] A;
 
 	return 0;
 }
