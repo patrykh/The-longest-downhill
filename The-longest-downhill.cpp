e6891d5dc87ce4b5adde7ad24df2da10629cb3b8
patrykh <holda.patryk@gmail.com>
2015-07-10 21:34:53 +0200

    Lista

--- The-longest-downhill.cpp
+++ The-longest-downhill.cpp
@@ -49,6 +49,12 @@ void usun(stos **root)
 	}
 }
 
+struct element_listy
+{
+	element_listy * next;
+	int v;
+};
+
 int main()
 {
 	int ilosc_wierzcholkow = 0,
@@ -59,48 +65,7 @@ int main()
 	
 	cin >> ilosc_wierzcholkow >> start;
 
-	//Tworzenie macierzy
-	char ** A = new char*[ilosc_wierzcholkow];
-	for (int i = 0; i < ilosc_wierzcholkow; i++){
-		A[i] = new char[ilosc_wierzcholkow];
-	}
-
-	//wypisywanie zerami
-	for (int i = 0; i < ilosc_wierzcholkow; i++){
-		for (int j = 0; j < ilosc_wierzcholkow; j++){
-			A[i][j] = 0;
-		}
-	}
-
-	//char ??
-	//for (int m = 0; m < ilosc_wierzcholkow; m++){
-	//	cin >> ilosc_sasiadow;
-	//	for (int n = 0; n < ilosc_sasiadow; n++){
-	//		cin >> sasiad >> waga;
-	//		a[m][sasiad] = '1';
-	//	}
-	//}
-
-	// INT ?? 
-	for (int m = 0; m < ilosc_wierzcholkow; m++){
-		cin >> ilosc_sasiadow;
-		for (int n = 0; n < ilosc_sasiadow; n++){
-			cin >> sasiad >> waga;
-			A[m][sasiad] = waga;
-		}
-	}
-
-	//wyœwietlanie macierzy
-	cout << "-   ";
-	for (int i = 0; i < ilosc_wierzcholkow; i++) cout << i << " ";
-	cout << endl << endl;
-	for (int i = 0; i < ilosc_wierzcholkow; i++){
-		cout << i << "   ";
-		for (int j = 0; j < ilosc_wierzcholkow; j++){
-			cout << A[i][j] << " ";
-		}
-		cout << "\n";
-	}
+	
 
 	//Czyszczenie pamiêci
 	for (int i = 0; i < ilosc_wierzcholkow; i++) delete[] A[i];
