4608325905b8c72104e3f63db5a99ae859d1245c
patrykh <holda.patryk@gmail.com>
2015-07-15 21:17:50 +0200

    BFS
    Usunięcie Algorytmu Dijsktry i zastąpienie go przeglądaniem wszerz
    (BFS).

--- The-longest-downhill.cpp
+++ The-longest-downhill.cpp
@@ -19,7 +19,6 @@ struct element_listy
 
 int main()
 {
-	const int MININT = -2147483647;
 	int ilosc_wierzcholkow = 0,
 		start = 0,
 		ilosc_sasiadow = 0,
@@ -28,29 +27,21 @@ int main()
 		sptr = 0,
 		j = 0,
 		u = 0,
-		i = 0,
-		*d,
-		*p,
-		*S;
-	bool *QS;
+		i = 0;
+	bool *visited;
 
-	element_listy **graf;
+	element_listy **lista_sasiedztwa;
 	element_listy *pw, *rw;
 
 	cin >> ilosc_wierzcholkow >> start;
 
-	d = new int[ilosc_wierzcholkow];
-	p = new int[ilosc_wierzcholkow];
-	QS = new bool[ilosc_wierzcholkow];
-	graf = new element_listy *[ilosc_wierzcholkow];
-	S = new int[ilosc_wierzcholkow];
-	
+	visited = new bool[ilosc_wierzcholkow];
+	lista_sasiedztwa = new element_listy *[ilosc_wierzcholkow];
+
 	//Inicjacja tablic dynamicznych
 	for (i = 0; i < ilosc_wierzcholkow; i++){
-		d[i] = MININT;
-		p[i] = -1;
-		QS[i] = false;
-		graf[i] = NULL;
+		visited[i] = false;
+		lista_sasiedztwa[i] = NULL;
 	}
 
 	// Dane wejœciowe
@@ -61,14 +52,15 @@ int main()
 			pw = new element_listy;
 			pw->v = sasiad;
 			pw->w = waga;
-			pw->next = graf[i];
-			graf[i] = pw;
+			pw->next = lista_sasiedztwa[i];
+			lista_sasiedztwa[i] = pw;
 		}
 	}
-	
-	////wyswietlanie - OK!
+
+	//wyswietlanie - OK!
+
 	//for (i = 0; i < ilosc_wierzcholkow; ++i){
-	//	pw = graf[i];
+	//	pw = lista_sasiedztwa[i];
 	//	cout << i << ' ';
 	//	while (pw){
 	//		cout << pw->v;
@@ -80,57 +72,10 @@ int main()
 	//	cout << endl;
 	//}
 
-	d[start] = 0;
-
-	for (i = 0; i < ilosc_wierzcholkow; i++){
-		// Szukamy wierzcho³ka w Q o najmniejszym koszcie d
-		for (j = 0; QS[j]; j++);
-		for (u = j++; j < ilosc_wierzcholkow; j++){
-			if (!QS[j] && (d[j] > d[u])) u = j;
-		}
-
-		// Znaleziony wierzcho³ek przenosimy do S
-		QS[u] = true;
-
-		// Modyfikujemy odpowiednio wszystkich s¹siadów u, którzy s¹ w Q
-		for (pw = graf[u]; pw; pw = pw->next){
-			if (!QS[pw->v] && (d[pw->v] < d[u] + pw->w)){ // Druga opcja to porównanie wag
-				d[pw->v] = d[u] + pw->v;
-				p[pw->v] = u;
-			}
-		}
-	}
-
-	// Gotowe, wyœwietlamy wyniki
-
-	for (i = 0; i < ilosc_wierzcholkow; i++)
-	{
-		cout << i << ": ";
-
-		// Œcie¿kê przechodzimy od koñca ku pocz¹tkowi,
-		// Zapisuj¹c na stosie kolejne wierzcho³ki
-
-		for (j = i; j > -1; j = p[j]) S[sptr++] = j;
-
-		// Wyœwietlamy œcie¿kê, pobieraj¹c wierzcho³ki ze stosu
-
-		while (sptr) cout << S[--sptr] << " ";
-
-		// Na koñcu œcie¿ki wypisujemy jej koszt
-
-		cout << "$" << d[i] << endl;
-	}
-
 	// Usuwamy tablice dynamiczne
-
-	delete[] d;
-	delete[] p;
-	delete[] QS;
-	delete[] S;
-
 	for (i = 0; i < ilosc_wierzcholkow; i++)
 	{
-		pw = graf[i];
+		pw = lista_sasiedztwa[i];
 		while (pw)
 		{
 			rw = pw;
@@ -139,7 +84,8 @@ int main()
 		}
 	}
 
-	delete[] graf;
+	delete[] lista_sasiedztwa;
+	delete[] visited;
 
 	return 0;
 }
