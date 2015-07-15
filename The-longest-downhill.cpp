0633c44d7b7eb4aca77acd5602d2e47984450e37
patrykh <holda.patryk@gmail.com>
2015-07-15 21:44:47 +0200

    BFS
    Dodany BFS

--- The-longest-downhill.cpp
+++ The-longest-downhill.cpp
@@ -6,7 +6,7 @@
 // Graf skierowany, wa¿ony
 // Alg Dijkstry z wagami ujemnymi???
 
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <iostream>
 
 using namespace std;
@@ -17,6 +17,31 @@ struct element_listy
 	int v, w;
 };
 
+void dodaj(int dana, element_listy ** koniec)
+{
+	element_listy * newElement = new element_listy;
+	newElement->v = dana;
+	(*koniec)->next = newElement;
+	(*koniec) = newElement;
+}
+
+void pobierz(element_listy ** koniec)
+{
+	element_listy * tmp = *koniec;
+	cout << "\n" << tmp->v << "\n";
+}
+
+void usun(element_listy **poczatek)
+{
+
+	element_listy * tmp = *poczatek;
+	*poczatek = (*poczatek)->next;
+
+	cout << (*poczatek)->v << "\n";
+
+	delete tmp;
+}
+
 int main()
 {
 	int ilosc_wierzcholkow = 0,
@@ -24,14 +49,15 @@ int main()
 		ilosc_sasiadow = 0,
 		sasiad = 0,
 		waga = 0,
-		sptr = 0,
+		droga = 0,
 		j = 0,
-		u = 0,
+		w = 0,
 		i = 0;
 	bool *visited;
 
 	element_listy **lista_sasiedztwa;
 	element_listy *pw, *rw;
+	element_listy *Q, *head, *tail;
 
 	cin >> ilosc_wierzcholkow >> start;
 
@@ -71,6 +97,42 @@ int main()
 	//	}
 	//	cout << endl;
 	//}
+
+	//BFS
+
+	Q = new element_listy;
+	Q->next = NULL;
+	Q->v = start;
+	head = tail = Q;
+
+	visited[start] = true;
+
+	while (head){
+		w = head->v;
+
+		Q = head;
+		head = head->next;
+		if (!head) tail = NULL;
+		delete Q;
+
+		cout << w << ' ';
+
+		for (pw = lista_sasiedztwa[w]; pw; pw = pw->next){
+			Q = new element_listy;
+			Q->next = NULL;
+			Q->v = pw->v;
+
+			if (!tail){
+				head = Q;
+			}else{
+				tail->next = Q;
+			}
+			tail = Q;
+			visited[pw->v] = true;
+		}
+	}
+
+
 
 	// Usuwamy tablice dynamiczne
 	for (i = 0; i < ilosc_wierzcholkow; i++)
